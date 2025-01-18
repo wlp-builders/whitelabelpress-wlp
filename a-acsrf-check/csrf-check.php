50c0ba6cbbc23abc6315311c3f0c47f97b4210a7
Neil <maartenq@protonmail.com>
2025-01-18 03:01:09 +0100

    made csrf-check stronger in wp-admin area

--- a-acsrf-check/csrf-check.php
+++ a-acsrf-check/csrf-check.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: CSRF Referrer Check with Site URL Validation
  * Description: A simple plugin to prevent CSRF attacks by checking the referer for POST requests, with special handling for /wp-admin and excluding admin-ajax.php.
- * Version: 1.2
+ * Version: 1.3
  * Author: Your Name
- * License: GPL2
+ * License: Spirit of Time 1.0
  */
 
 if (!defined('ABSPATH')) {
@@ -19,25 +19,33 @@ function handle_csrf_with_referrer() {
 	if (php_sapi_name() != 'cli' && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if (isset($_SERVER['HTTP_DID'])) {
-            return;  // Skip the CSRF checks for DID.json clients
+                            log_message('[csrf check]: skipped HTTP DID');
+                            return;  // Skip the CSRF checks for DID.json clients
+
         }
 
         if (isset($_SERVER['HTTP_WLP_AUTHORIZATION'])) {
+                log_message('[csrf check]: skipped WLP AUTH');
             return;  // Skip the CSRF checks for headless apps
         }
 
         // Check for admin-ajax.php
         if (strpos($_SERVER['REQUEST_URI'], 'admin-ajax.php') !== false) {
-            check_site_url();  // Validate referer for admin-ajax.php
+        log_message('[csrf check]: admin-ajax.php');
+        check_site_url();  // Validate referer for admin-ajax.php
         } 
         // Check for /wp-admin area
         elseif (strpos($_SERVER['REQUEST_URI'], '/wp-admin') !== false) {
+        log_message('[csrf check]: /wp-admin');
             check_site_url_with_wp_admin(); // Validate referer for /wp-admin
         }
         // Check for other POST requests
         else {
+        log_message('[csrf check]: else');
             check_site_url();  // Validate referer for general POST requests
         }
+    } else {
+    	log_message('[csrf check]: skipped');
     }
 }
 
@@ -68,7 +76,7 @@ function check_site_url_with_wp_admin() {
     $referer = $_SERVER['HTTP_REFERER'] ?? '';
     $site_url = site_url(); // Get the full site URL (including scheme and domain)
     
-    if (strpos($referer, $site_url) !== 0) {
+    if (strpos($referer, $site_url.'/wp-admin') !== 0) {
         // Log the CSRF attempt (optional)
         error_log('CSRF detected: invalid referer for /wp-admin request: ' . $referer);
         
@@ -83,5 +91,6 @@ function check_site_url_with_wp_admin() {
 
 // Hook the CSRF check into WordPress's init action
 add_action('init', 'handle_csrf_with_referrer');
+
 
 
